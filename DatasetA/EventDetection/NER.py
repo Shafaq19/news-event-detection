76ea976852fc509b88cc879f50c669cd8d5935f7
Shafaq Arshad <arshad.shafaq15@gmail.com>
2019-06-13 07:54:04 +0000

    NER.py deleted online with Bitbucket

--- DatasetA/EventDetection/NER.py
+++
@@ -1,7 +0,0 @@
-from MainFolder.EventDetection.DynammicClustering import DynammicClustering
-
-clusteringAlg = DynammicClustering(a=float(5 / 11), b=float(0 / 11),
-                                   c=float(1 / 11), d=float(5 / 11), threshold=0.1, threshold2=0.2, threshold3=.5,
-                                   inputFile="../Data/mytweet15 May.csv", Outfilename="../MyOutputs/clustersIds.csv")
-
-print(clusteringAlg.NERPass("hey I am shafaq ;p;p whats up? wth",0))
